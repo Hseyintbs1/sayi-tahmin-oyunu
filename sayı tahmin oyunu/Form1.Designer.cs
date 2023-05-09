bdd01e81a64052592071971bd9ca10f8c62baa2b
Hseyintbs1 <220716044@ogr.cbu.edu.tr>
2023-05-09 18:37:11 +0300

    Add project files.

---
+++ sayı tahmin oyunu/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace sayı_tahmin_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.KucukSayi = new System.Windows.Forms.NumericUpDown();
+            this.BuyukSayi = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.KucukSayi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuyukSayi)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sayı Üret";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tahmin Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // KucukSayi
+            // 
+            this.KucukSayi.Location = new System.Drawing.Point(6, 19);
+            this.KucukSayi.Name = "KucukSayi";
+            this.KucukSayi.Size = new System.Drawing.Size(47, 20);
+            this.KucukSayi.TabIndex = 6;
+            this.KucukSayi.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // BuyukSayi
+            // 
+            this.BuyukSayi.Location = new System.Drawing.Point(83, 19);
+            this.BuyukSayi.Name = "BuyukSayi";
+            this.BuyukSayi.Size = new System.Drawing.Size(47, 20);
+            this.BuyukSayi.TabIndex = 7;
+            this.BuyukSayi.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "İle";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.BuyukSayi);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.KucukSayi);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 82);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sayı tahmin oyunu için hangi aralıkta sayı üretilsin?";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(266, 102);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rastgele üretilen sayıyı tahmin ediniz?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 214);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.KucukSayi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuyukSayi)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown KucukSayi;
+        private System.Windows.Forms.NumericUpDown BuyukSayi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ sayı tahmin oyunu/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace sayı_tahmin_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        int kucuksayi,
+            buyuksayi,
+            S1,
+            can,
+            puan = 0;
+            
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            kucuksayi = Convert.ToUInt16(KucukSayi.Value);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KucukSayi.Value = 0;
+            BuyukSayi.Value = 0;
+            can = 0;
+            puan= 0;
+            S1= 0;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+
+        }
+
+        
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+             
+            buyuksayi = Convert.ToUInt16(BuyukSayi.Value);
+
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             can = buyuksayi - kucuksayi;
+
+
+            if (can == 0 )
+            {
+                MessageBox.Show("İki sayı eşit olamaz!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Form_Update();
+            }
+            else if (can < 0 )
+            {
+                
+                MessageBox.Show("Birinci(soldaki) sayı İkinci sayidan büyük olamaz!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Form_Update();
+            }
+            else
+            {
+        
+                Random rnd = new Random();
+                S1 = rnd.Next(kucuksayi , buyuksayi);
+        
+                
+                label4.Text = Convert.ToString("Kalan deneme hakkı = " + can);
+                label1.Text = Convert.ToString(kucuksayi) + " İle " + Convert.ToString(buyuksayi) + " arasında rastgele bir sayı oluşturuldu." + S1;
+            }
+         
+        }
+
+        
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+           
+            
+                if (Convert.ToInt32(textBox1.Text) == S1 )
+                {
+                    label3.Text = "Tebrikler Doğru bildiniz!";
+                    puan += 10;
+                    label2.Text = Convert.ToString("puan = " + puan);
+                    can = can - 1;
+                    ResetForm();
+                }
+                else
+                {
+                    label3.Text = "Tekrar deneyiniz!";
+                    puan -= 2;
+                    label2.Text = Convert.ToString("puan = " + puan);
+                    can = can - 1;
+                }
+
+                label4.Text = Convert.ToString("Kalan deneme hakkı = " + can);
+
+            if (can == 0 || can < 0)
+            {
+                DialogResult result = MessageBox.Show("Kalan deneme hakkı bitti, oyunu sıfırlamak istiyor musunuz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                {
+                    ResetForm();
+                }
+                else
+                {
+                    Form_Update();
+                }
+
+            }
+            
+        }
+
+        private void ResetForm()
+        {
+            // Tüm kontrol değerlerini sıfırla veya varsayılan değerlerine geri dön.
+            KucukSayi.Value = 0;
+            BuyukSayi.Value = 0;
+            S1 = 0;
+            can = 0;
+            // vb.
+
+            // Yapılandırılan özel kodları burada da ekleyebilirsiniz.
+        }
+
+        private void Form_Update() 
+        {
+            KucukSayi.Value = 0;
+            BuyukSayi.Value = 0;
+            can = 0;
+           
+            S1 = 0;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+
+
+        }
+
+
+    }
+}
